9618cd62fab0931b79749622fafa04533ae54558
Thilina Prasad <thilinaprasad.15@cse.mrt.ac.lk>
2018-02-18 20:07:50 +0530

    show contest - dynamic content

--- resources/views/contests/show.blade.php
+++ resources/views/contests/show.blade.php
@@ -14,7 +14,7 @@
             <div class="card-img-overlay show-overlay text-center">
                 <!--Circular Progress-->
                 <div class="col-sm-1 mx-auto d-block">
-                    <div class="progress" data-percentage="70">
+                    <div class="progress" data-percentage="{{ $days_presentage }}">
                         <span class="progress-left">
                                         <span class="progress-bar"></span>
                         </span>
@@ -23,56 +23,49 @@
                         </span>
                         <div class="progress-value">
                             <div>
-                                5<br>
+                                {{$days_left}}<br>
                                 <span>days left</span>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!--Circular Progress-->
-                <h1 class="card-title display-3">Name</h1>
-                <h3 class="prize">Win amazing</h3>
+                <h1 class="card-title display-3">{{ $contest->title }}</h1>
+                <h3 class="prize">Try With Your Creativity & Win<p> <strong class="h1">{{ $contest->prize }} </strong></h3>
                 <a href="#" class="btn btn-light font_03"><i class="fas fa-upload"></i>&nbsp;&nbsp;Submit Photographs</a>
             </div>
         </div>
     </div>
 </div>
 <!--cover Section-->
+
 <!--about section-->
 <div class="jumbotron jumbotron-fluid text-center show-about">
     <div class="container">
-        <p class="h1 font_01">About Title Here</p>
-        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        <p class="h1 font_01">About {{ $contest->title }} Contest</p>
+        <p class="lead">{{ $contest->description }}</p>
     </div>
 </div>
 <!--about section-->
 <!--prizes section-->
 <div class="jumbotron text-center prize-section">
     <p class="font_01 h1 prize-head"><i class="fas fa-chess-queen"></i> Win Amazing Prize <i class="fas fa-chess-queen"></i></p>
-    <p class="font_03 h3">Canon 6d</p>
-    <img src="{{ asset('img/prize.jpg') }}" class="hover-effect-2 mx-auto d-block img-fluid prize_img rounded-circle">
-    <p class="col-md-6 offset-md-3" style="margin-top:20px;">It's not about the prizes, but...the prizes do rock. From getting published to scoring Canon 5Ds, winning contests isn't
-        just about bragging rights. Elevate your photography and get inspired!</p>
+    <p class="font_03 h3">{{ $contest->prize }}</p>
+    <img src="/storage/contests_prizes/{{ $contest->prize_image }}" class="hover-effect-2 mx-auto d-block img-fluid prize_img rounded-circle">
+    <p class="col-md-6 offset-md-3" style="margin-top:20px;">{{ $contest->prize_description }}</p>
 </div>
 <!--prizes section-->
 <!--Partners Section-->
 <div class="jumbotron marketing text-center show-partner">
         <p class="h1 font_01"><i class="fas fa-handshake"></i> Partnesr with us <i class="fas fa-handshake"></i></p>
     <div class="row">
+        @foreach($sponsors as $sponsor)
         <div class="col-lg-4">
-            <img class="rounded-circle hover-effect-2" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image"
+            <img class="rounded-circle hover-effect-2" src="/storage/contests_sponsors/{{ $sponsor->logo }}" alt="Generic placeholder image"
                 width="140" height="140">
-            <h3>Partner 1</h3>
+            <h3>{{ $sponsor->name }}</h3>
         </div>
-        <div class="col-lg-4">
-            <img class="rounded-circle hover-effect-2" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image"
-                width="140" height="140">
-            <h3>Partner 2</h3>
-        </div>
-        <div class="col-lg-4">
-            <img class="rounded-circle hover-effect-2" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image"
-                width="140" height="140">
-            <h3>Partner 3</h3>
+@endforeach
         </div>
     </div>
 </div>
@@ -83,8 +76,8 @@
     <div class="text-center">
         <strong>Organized by</strong>
     <a href="#" class="organizer">
-        <img src="{{ asset('img/prize.jpg') }}" class="rounded-circle mx-auto d-block" width="80" height="80" title="View Profile">
-        <strong>Thilina Prasad</strong>
+        <img src="/storage/profile_pics/{{ $owner->profile_pic}}" class="rounded-circle mx-auto d-block" width="80" height="80" title="View Profile">
+        <strong>{{ $owner->name}}</strong>
     </a>
 </div>
 </div>
